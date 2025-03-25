f49ee56f6503fe8ed5e12bd5c242a77838f03c84
arunaraavi522 <arunajyothi.raavi@cignahealthcare.com>
2025-03-25 15:00:20 -0500

    Update accordion.tsx
    Signed-off-by: arunaraavi522 <arunajyothi.raavi@cignahealthcare.com>

--- components/collapse/demo/accordion.tsx
+++ components/collapse/demo/accordion.tsx
@@ -3,7 +3,7 @@ import type { CollapseProps } from 'antd';
 import { Collapse } from 'antd';
 
 const text = `
-  A dog is a type of domesticated animal.
+  A is a type of domesticated animal.
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
